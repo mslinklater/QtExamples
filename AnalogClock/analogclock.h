dca118f16b4afc1b1099e845436fdc0669fe0652
Martin Linklater <mslinklater@gmail.com>
2018-03-19 22:21:22 +0000

    Added analog clock example

---
+++ AnalogClock/analogclock.h
@@ -0,0 +1,18 @@
+#ifndef ANALOGCLOCK_H
+#define ANALOGCLOCK_H
+
+#include <QWidget>
+
+class AnalogClock : public QWidget
+{
+    Q_OBJECT
+
+public:
+    AnalogClock(QWidget *parent = 0);
+    ~AnalogClock();
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+};
+
+#endif // ANALOGCLOCK_H
